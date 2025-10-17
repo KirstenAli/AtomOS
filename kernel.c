5e6d7501a11ace0131f3063735b2f928ac4d79c8
Kirsten <ikirstenhd@gmail.com>
2025-10-17 19:08:42 +0100

    print function added

--- kernel.c
+++ kernel.c
@@ -6,7 +6,7 @@
 #define COLOR 0x0F  /* bright white on black */
 
 static inline uint16_t make_cell(char c, uint8_t color) {
-    return (uint16_t)c | ((uint16_t)color << 8);
+    return (uint16_t) c | ((uint16_t) color << 8);
 }
 
 static inline void clear_screen(uint8_t color) {
@@ -20,13 +20,14 @@ static inline void halt_forever(void) {
     for (;;) __asm__ __volatile__("hlt");
 }
 
+static inline void print(const char *s) {
+    for (int i = 0; s[i]; i++) {
+        VGA[i] = make_cell(s[i], COLOR);
+    }
+}
+
 void kmain(void) {
     clear_screen(COLOR);
-
-    const char* msg = "AtomOS (C): Hello, world!";
-    for (int i = 0; msg[i]; i++) {
-        VGA[i] = make_cell(msg[i], COLOR);
-    }
-
+    print("AtomOS (C): Hello, world!");
     halt_forever();
 }
